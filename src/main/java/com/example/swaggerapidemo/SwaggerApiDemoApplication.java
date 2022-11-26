448dc39726b1e8210883cc8b35ddf5b3c7f1d985
unknown <iamadamhussain@gmail.com>
2022-11-26 06:43:09 +0530

    swagger api

---
+++ src/main/java/com/example/swaggerapidemo/SwaggerApiDemoApplication.java
@@ -0,0 +1,30 @@
+package com.example.swaggerapidemo;
+
+import static springfox.documentation.spi.DocumentationType.SWAGGER_2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@EnableSwagger2
+@SpringBootApplication
+public class SwaggerApiDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwaggerApiDemoApplication.class, args);
+	}
+	  @Bean
+	    public Docket swagger() {
+	        return new Docket(SWAGGER_2)
+	            .select()
+	            .apis(RequestHandlerSelectors.any())
+	            .paths(PathSelectors.any())
+	            .build();
+	    }
+
+}
